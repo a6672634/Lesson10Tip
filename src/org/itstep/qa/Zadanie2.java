ccf24cf201ddffc8cdd1e1bacf27562d39f0f6c7
a6672634 <a6672634@yandex.ru>
2018-09-14 12:45:36 +0300

    HomeWork

--- src/org/itstep/qa/Zadanie2.java
+++ src/org/itstep/qa/Zadanie2.java
@@ -9,43 +9,68 @@ public class Zadanie2 {
         Scanner scanner = new Scanner(System.in);
         int str = scanner.nextInt();
         switch (str) {
-            case "январь" : {
-
-            }
-            case "март" : {
-
-            }
-            case "май" : {
-
-            }
-            case "июнь" : {
-
-            }
-            case "август" : {
-
-            }
-            case "октябрь" : {
-
-            }
-
-            case "декабрь" : {
-                System.out.println("30 дней");
+            case 30 : {
+                System.out.println("январь");
+                System.out.println("март");
+                System.out.println("май");
+                System.out.println("июнь");
+                System.out.println("август");
+                System.out.println("октябрь");
+                System.out.println("декабрь");
                 break;
             }
-            case "апрель": {
+            case 31: {
+                System.out.println("апрель");
+                System.out.println("июль");
+                System.out.println("сентябрь");
+                System.out.println("ноябрь");
+                break;
             }
-            case "июль": {
+            case 28: {
+                System.out.println("январь");
+                System.out.println("март");
+                System.out.println("май");
+                System.out.println("июнь");
+                System.out.println("август");
+                System.out.println("октябрь");
+                System.out.println("декабрь");
+                System.out.println("апрель");
+                System.out.println("июль");
+                System.out.println("сентябрь");
+                System.out.println("ноябрь");
+                System.out.println("февраль");
 
+                break;
             }
-            case "сентябрь" : {
+            case 29: {
+                System.out.println("январь");
+                System.out.println("март");
+                System.out.println("май");
+                System.out.println("июнь");
+                System.out.println("август");
+                System.out.println("октябрь");
+                System.out.println("декабрь");
+                System.out.println("апрель");
+                System.out.println("июль");
+                System.out.println("сентябрь");
+                System.out.println("ноябрь");
+                System.out.println("февраль");
 
-            }
-            case "ноябрь" : {
-                System.out.println("31 день");
                 break;
             }
             default:{
-                System.out.println("28");
+                System.out.println("январь");
+                System.out.println("март");
+                System.out.println("май");
+                System.out.println("июнь");
+                System.out.println("август");
+                System.out.println("октябрь");
+                System.out.println("декабрь");
+                System.out.println("апрель");
+                System.out.println("июль");
+                System.out.println("сентябрь");
+                System.out.println("ноябрь");
+                System.out.println("февраль");
             }
         }
     }
